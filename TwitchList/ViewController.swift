ee197df6c57c1132c839774368432434ad353321
dedeexe <dede.exe@gmail.com>
2017-05-01 11:26:51 -0300

    Initial Project Commit

---
+++ TwitchList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitchList
+//
+//  Created by dede.exe on 01/05/17.
+//  Copyright © 2017 dede.exe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
